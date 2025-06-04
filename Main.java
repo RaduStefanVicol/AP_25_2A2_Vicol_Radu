04d5a7165472eefeccafb01e31cd73edf1209cb3
RaduStefanVicol <radustefanvicol@gmail.com>
2025-06-04 18:06:07 +0300

    Fix compulsory

--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class<?> classUseClass = Class.forName("org.example.UseClass");
-        UseClass object1 = new UseClass();
 
-        System.out.println("Methods in UseClass:");
+        System.out.println("Metode in UseClass:");
         for (Method method : classUseClass.getDeclaredMethods()) {
             System.out.println("  " + method);
         }
-        System.out.println("Utilizare metode:\n");
-        
+
+        System.out.println("\nUtilizare metode:\n");
+
+        for (Method method : classUseClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Test.class)) {
+                if (Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0) {
+                    System.out.println("Invocare metoda test: " + method.getName());
+                    try {
+                        method.invoke(null);
+                    } catch (Exception e) {
+                        System.out.println("Eroare la invocare: " + e.getMessage());
+                    }
+                }
+            }
+        }
     }
 }
--- UseClass.java
+++ UseClass.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class UseClass {
-    static void printHello(){
+    public static void staticHello(){
+        System.out.println("Hello");
+    }
+
+    @Test
+    public static void testStaticHello(){
+        System.out.println("Hello");
+    }
+
+    public void simpleHello(){
+        System.out.println("Hello");
+    }
+
+    @Test
+    public void testHello(){
         System.out.println("Hello");
     }
 }
